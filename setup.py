1f9bedfa0a9aef68b5cd46b851630dccbc5aecf1
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-14 12:45:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ColabTurtlePlus',
     version='1.0.0',
     packages=['ColabTurtlePlus'],
-    url='https://github.com/mathriddle/MyColabTurtle',
+    url='https://github.com/mathriddle/ColabTurtlePlus',
     license='MIT',
     author='Larry Riddle',
     author_email='lriddle@agnesscott.edu',
