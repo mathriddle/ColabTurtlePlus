7667fc7b2253ac67ecba6bcbc1ea8c0954f37344
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-13 21:14:33 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ColabTurtle',
+    version='3.0.0',
+    packages=['ColabTurtle'],
+    url='https://github.com/mathriddle/MyColabTurtle',
+    license='MIT',
+    author='Larry Riddle',
+    author_email='lriddle@agnesscott.edu',
+    description='An HTML based Turtle implementation for Google Colab and Jupyter Labs'
+)
