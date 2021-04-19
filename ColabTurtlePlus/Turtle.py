ccf962d46eabbd94c2bb150b1552c6a1f35d59e9
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-18 21:28:45 -0400

    add degrees change to square

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -242,6 +242,7 @@ def _generateTurtleSvgDrawing():
         degrees -= 90
         template = TURTLE_ARROW_SVG_TEMPLATE
     elif turtle_shape == 'square':
+        degrees -= 90
         template = TURTLE_SQUARE_SVG_TEMPLATE
 
     return template.format(turtle_color=fill_color,
