968f1bb21a1b9d6c1957af1a24438733ef4534f9
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-29 14:55:49 -0400

    Update Turtle.py

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -563,12 +563,13 @@ def right(degrees):
         raise ValueError('Degrees must be a number.')
     turtle_degree_orig = turtle_degree
     temp = TURTLE_TURTLE2_SVG_TEMPLATE
-    TURTLE_TURTLE2_SVG_TEMPLATE = TURTLE_TURTLE2_SVG_TEMPLATE.replace("</g>","""<animateTransform attributeName="transform"
+    tmp = """<animateTransform attributeName="transform"
                     type="rotate"
                     from="{st} {x} {y}" to ="{lt} {x} {y}"
                     begin="0s" dur="5s"
                     repeatCount="1"
           /></g>""".format(st=0, lt=degrees, x=turtle_pos[0],y=turtle_pos[1])
+    TURTLE_TURTLE2_SVG_TEMPLATE = TURTLE_TURTLE2_SVG_TEMPLATE.replace("</g>",tmp)
     _updateDrawing()
     print(TURTLE_TURTLE2_SVG_TEMPLATE)
     turtle_degree = (turtle_degree_orig + degrees) % 360
