a2d291e995a42cd44cafe032cb102599d42b84fb
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-04-30 17:54:43 -0400

    Update Turtle.py

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -331,10 +331,34 @@ def _moveToNewPosition(new_pos):
 
     # rounding the new_pos to eliminate floating point errors.
     new_pos = ( round(new_pos[0],3), round(new_pos[1],3) )
-    
     start_pos = turtle_pos
+    dist = (new_pos[0]-start_pos[0])^2 + (new_pos[1]-start_pos[1])^2
   
     if is_pen_down:
+        stretchfactor_orig = stretchfactor
+        template = shapeDict[turtle_shape]        
+        tmp = """<animate attributeName="x"
+            attributeType="XML"
+            from="{sx}" to="{ex}"
+            begin="0s" dur="{t}s"
+            repeatCount="1"
+           />
+   <animate attributeName="y"
+            attributeType="XML"
+            from="{sy}" to="{ey}"
+            begin="0s" dur="{t}s"
+            fill="freeze"
+            repeatCount="1"
+           />
+          /></g>""".format(sx=start_pos[0],ex=new_pos[0],sy=start_pos[1],t=timeout)
+        newtemplate = template.replace("</g>",tmp)
+        shapeDict.update({turtle_shape:newtemplate})
+        stretchfactor = 1,1
+        #timeout = timeout*abs(degrees)/90+0.001
+        _updateDrawing()
+        shapeDict.update({turtle_shape:template})
+        stretchfactor = stretchfactor_orig
+        #timeout = timeout_orig
         svg_lines_string += \
             """<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke-linecap="round" style="stroke:{pen_color};stroke-width:{pen_width}" />""".format(
                         x1=start_pos[0],
