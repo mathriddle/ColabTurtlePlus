a37e46fc9c22ca6d6dd20e3ef42d140af5d12d10
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-07-26 19:34:49 -0400

    Update Turtle.py

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -466,7 +466,6 @@ def _moveToNewPosition(units):
         # create temporary svg string to show the animation
         #initial_pos=[xmin + turtle_pos[0]/xscale,ymax - turtle_pos[1]/yscale]
         initial_pos = position()
-        print(initial_pos)
         alpha = math.radians(turtle_degree)
         timeout = timeout*0.25
         tenx, teny = units/10, units/10
@@ -475,7 +474,6 @@ def _moveToNewPosition(units):
             dx = min(tenx,s*units)
             dy = min(teny,s*units)
             turtle_pos = (initial_pos[0] + s * dx * math.cos(alpha), initial_pos[1] + s * dy * math.sin(alpha))
-            print(turtle_pos,_convertx(turtle_pos[0]),_converty(turtle_pos[1]))
             if is_pen_down:
                 svg_lines_string += \
                     """<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke-linecap="round" style="stroke:{pen_color};stroke-width:{pen_width}" />""".format(
@@ -486,6 +484,7 @@ def _moveToNewPosition(units):
                         pen_color=pen_color, 
                         pen_width=pen_width) 
             initial_pos = turtle_pos
+            print(svg_lines_string)
             _updateDrawing()
             units -= dunits
     if is_pen_down:
