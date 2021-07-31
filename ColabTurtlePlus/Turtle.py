bff0a4a0caa44934936196ce7967ea96f4ed4725
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-07-31 16:16:08 -0400

    Update Turtle.py

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -2354,6 +2354,6 @@ def _turtleOrientation():
     alpha = math.radians(heading())
     Dxy = (_convertx(getx()+math.cos(alpha))-_convertx(getx()),_converty(gety()+math.sin(alpha))-_converty(gety()))
     deg = math.degrees(math.atan2(-Dxy[1],Dxy[0])) % 360
-    print("orient"
+    print("orient")
     return 360-deg
 
