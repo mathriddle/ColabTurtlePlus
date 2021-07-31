d77360c676337a89f7155d4ea7e915ffa70bd612
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-07-31 12:26:59 -0400

    Update Turtle.py

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -316,7 +316,7 @@ def _generateTurtleSvgDrawing():
 
     turtle_x = turtle_pos[0]
     turtle_y = turtle_pos[1]
-    if _mode == "world"
+    if _mode == "world":
         degrees = turtle_orient - tilt_angle
     elif _mode == "standard":
         degrees = turtle_degree - tilt_angle
