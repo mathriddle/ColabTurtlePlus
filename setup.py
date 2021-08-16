a17cbdbe6ae76f280624102d7cbaaba57d14ef2c
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-08-15 20:30:37 -0400

    update version to 1.4.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='ColabTurtlePlus',
-    version='1.4.3',
+    version='1.4.4',
     packages=['ColabTurtlePlus'],
     url='https://github.com/mathriddle/ColabTurtlePlus',
     license='MIT',
