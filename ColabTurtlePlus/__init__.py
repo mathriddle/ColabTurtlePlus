6bb4040e84e9e5a92976b726b77070711141ecf4
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-10-06 15:43:26 -0400

    Update __init__.py

--- ColabTurtlePlus/__init__.py
+++ ColabTurtlePlus/__init__.py
@@ -1,1 +1,1 @@
-print("Put clearscreen() as the first line to re-run turtle commands in a cell")
+print("Put clearscreen() as the first line in a cell (after the import command) to re-run turtle commands in the cell")
