6c9cca0d1a8028cdce0e2179b9f5ce62d93fbb4a
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2021-10-06 14:05:09 -0400

    Update Turtle.py

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -626,7 +626,7 @@ class _Screen:
         for turtle in self._turtles:
             turtle.reset()
     
-    #clearscreen = clear
+    clearscreen = clear
     resetscreen = reset
 
     # Set turtle mode (“standard”, “logo”, “world”, or "svg") and reset the window. If mode is not given, current mode is returned.
