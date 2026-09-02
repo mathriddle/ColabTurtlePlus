9d7953409d198e323b42d0214b819580a8b81808
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2026-09-02 14:28:06 -0400

    Update Turtle.py
    fixed missing )

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -222,7 +222,7 @@ class _Screen:
                            pw = turtle.outline_width,
                            rotation_x=turtle.turtle_pos[0], 
                            rotation_y=turtle.turtle_pos[1],
-                           points=pointsDict[turtle.turtle_shape]
+                           points=pointsDict[turtle.turtle_shape])
         return svg
     
     # helper function for linking svg strings of text
