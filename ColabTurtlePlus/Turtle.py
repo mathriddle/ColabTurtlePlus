b1f75bb8682acbb240739dd10c6d610180fbaaa0
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2026-09-08 17:37:57 -0400

    Update Turtle.py
    testing if scaling animation can be removed from right()

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -1052,14 +1052,7 @@ class RawTurtle:
         elif self.turtle_shape != 'ring' and self.stretchfactor[0]==self.stretchfactor[1]:
             stretchfactor_orig = self.stretchfactor
             template = shapeDict[self.turtle_shape]        
-            tmp = """<animateTransform id = "one" attributeName="transform" 
-                      type="scale"
-                      from="{sx} {sy}" to="{sx} {sy}"                      
-                      begin="0s" dur="0.01s"
-                      repeatCount="1"
-                      additive="sum"
-                      fill="freeze"
-                /><animateTransform attributeName="transform"
+            tmp = """<animateTransform attributeName="transform"
                     type="rotate"
                     from="0 0 0" to ="{extent} 0 0"
                     begin="one.end" dur="{t}s"
