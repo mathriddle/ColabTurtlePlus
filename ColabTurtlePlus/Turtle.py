fd6dbedae59211ffb4c2cf6ae6341a300ebfca9e
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2026-09-18 17:48:00 -0400

    Update Turtle.py
    give each turtle its own copy of the shapeDict. This is try to fix a bug when two turtles have the same name.

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -218,7 +218,7 @@ class _Screen:
             degrees -= 90
         
        
-        svg = shapeDict[turtle.turtle_shape].format(
+        svg = self.shapeDict[turtle.turtle_shape].format(
                            turtle_color=turtle.fill_color,
                            pcolor=turtle.pen_color,
                            turtle_x=turtle_x, 
@@ -1019,8 +1019,8 @@ class RawTurtle:
         self.stampnum = 0
         self.stamplist=[]
         self.points = DEFAULT_POINTS            
-
-        if screen._mode == "svg": shapeDict.update({"circle":TURTLE_RING_SVG_TEMPLATE})                                          
+        self.shapeDict = shapeDict.copy()
+        if screen._mode == "svg": self.shapeDict.update({"circle":TURTLE_RING_SVG_TEMPLATE})                                          
         screen._add(self)
         
         
@@ -1096,7 +1096,7 @@ class RawTurtle:
             self.screen._updateDrawing(turtle=self)
         elif self.turtle_shape != 'ring' and self.stretchfactor[0]==self.stretchfactor[1]:
             stretchfactor_orig = self.stretchfactor
-            template = shapeDict[self.turtle_shape]        
+            template = self.shapeDict[self.turtle_shape]        
             tmp = """<animateTransform id = "one" attributeName="transform" 
                       type="scale"
                       from="{sx} {sy}" to="{sx} {sy}"                      
@@ -1113,13 +1113,13 @@ class RawTurtle:
                     fill="freeze"
                 /></g>""".format(extent=deg, t=self.timeout*abs(deg)/90, sx=self.stretchfactor[0], sy=self.stretchfactor[1])
             newtemplate = template.replace("</g>",tmp)
-            shapeDict.update({self.turtle_shape:newtemplate})
+            self.shapeDict.update({self.turtle_shape:newtemplate})
             self.stretchfactor = 1,1
             self.timeout = self.timeout*abs(deg)/90+0.001
             self.screen._updateDrawing(self)
             self.turtle_degree = (self.turtle_degree + deg) % 360
             self.turtle_orient = self._turtleOrientation()
-            shapeDict.update({self.turtle_shape:template})
+            self.shapeDict.update({self.turtle_shape:template})
             self.stretchfactor = stretchfactor_orig
             self.timeout = timeout_orig
         else: #_turtle_shape == 'ring' or _stretchfactor[0] != _stretchfactor[1]
@@ -1772,7 +1772,7 @@ class RawTurtle:
             return 360-deg
 
     def extract_points(self):
-        svg_string = shapeDict[self.turtle_shape]
+        svg_string = self.shapeDict[self.turtle_shape]
         match = re.search(r'points="([^"]*)"', svg_string)
         if not match:
             raise ValueError("No points attribute found")
