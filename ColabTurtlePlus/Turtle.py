e6fceaef752ad485c18ffb33d44a160d5fe8ca0d
Larry Riddle <46355923+mathriddle@users.noreply.github.com>
2026-09-23 21:07:44 -0400

    Update Turtle.py

--- ColabTurtlePlus/Turtle.py
+++ ColabTurtlePlus/Turtle.py
@@ -890,8 +890,8 @@ class _Screen:
                 color = color.replace(" ", "")
             if not self._validateColorString(color):
             # Use an f-string
-                err = f'Color {color} is invalid. It can be a known html color name, 3-6 digit hex string, or rgb string.'
-                err2 = "See from https://www.w3schools.com/colors/colors_names.asp for known html color names"
+                err = f'Color {color} is invalid. It can be a known html color name, 3 or 6 digit hex string, or rgb string.'
+                err2 = "See https://www.w3schools.com/colors/colors_names.asp for known html color names."
                 raise ValueError(err + "\n" + err2)
             return color        
         elif isinstance(color, tuple):
